f60484ffeae74a41b7de731ac4b8b0e122a0e3bd
yousseffarmawy <yousseffarmawy22@gmail.com>
2025-07-23 15:28:34 +0200

    Add files via upload

---
+++ middleware/uploadmiddleware.js
@@ -0,0 +1,19 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "application/pdf") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only PDF files allowed!"), false);
+  }
+};
+
+const upload = multer({ storage, fileFilter, limits: { fileSize: 5 * 1024 * 1024 } });
+
+module.exports = upload;
